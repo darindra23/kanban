0f6f43563f9ffafb9b2db23bb9efdb0d0e3ec0ca
darindra23 <58590046+darindra23@users.noreply.github.com>
2020-03-10 15:03:55 +0700

    - Penambahan Fitur Google Login

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,6 +2,9 @@ const { User } = require("../models");
 const { sign } = require("../helpers/jwt");
 const { compare } = require("../helpers/bcrypt");
 const createError = require("http-errors");
+const { OAuth2Client } = require("google-auth-library");
+const generator = require("generate-password");
+const client = new OAuth2Client(process.env.CLIENT_ID);
 
 class Controller {
   static async register(req, res, next) {
@@ -27,6 +30,39 @@ class Controller {
         res.status(200).json({ access_token });
       } else {
         throw createError(400);
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+  static async google(req, res, next) {
+    try {
+      const ticket = await client.verifyIdToken({
+        idToken: req.body.id_token,
+        audience: process.env.CLIENT_ID
+      });
+      let { name, email } = ticket.getPayload();
+      let password = generator.generate({ length: 10, numbers: true });
+      let condition = {
+        where: {
+          email
+        }
+      };
+      let user = await User.findOne(condition);
+      if (!user) {
+        let { id, email } = await User.create({
+          fullname: name,
+          email,
+          password
+        });
+        let access_token = sign({ id, email });
+        res.status(201).json({ access_token });
+      } else {
+        let access_token = sign({
+          id: user.id,
+          email: user.email
+        });
+        res.status(200).json({ access_token });
       }
     } catch (error) {
       next(error);
--- server/migrations/20200310055123-create-user.js
+++ server/migrations/20200310055123-create-user.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
+    return queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -9,13 +9,17 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       fullname: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
@@ -28,6 +32,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
+    return queryInterface.dropTable("Users");
   }
 };
