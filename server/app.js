82ee7a732bd962d482b7dfebbdf139d2f24f2699
darindra23 <58590046+darindra23@users.noreply.github.com>
2020-03-15 11:50:23 +0700

    sedikit revisi error

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 if (process.env.NODE_ENV === "development") require("dotenv").config();
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const routes = require("./routes");
-const app = express();
 const PORT = process.env.PORT;
 const { errorHandler } = require("./middlewares/errorHandler");
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log("Listening On Port:", PORT);
+io.on("connection", socket => {
+  console.log("a user connected");
+  socket.on("show-data", data => {
+    socket.broadcast.emit("realtime-data", data);
+  });
 });
+
+http.listen(PORT, () => console.log(`listening on port:`, PORT));
